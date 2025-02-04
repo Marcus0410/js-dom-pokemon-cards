be877c6116601a2fa827037a4e20c783810a80fc
Marcus0410 <marcus.ikdal@gmail.com>
2025-02-04 13:34:39 +0100

    extension for toggling image

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 const cardList = document.querySelector(".cards")
 
+console.log(data[0])
+
 function createCard(pokemon) {
     // create elements
     const pokemonCard = document.createElement("li")
     const header = document.createElement("h2")
     const image = document.createElement("img")
 
+    const images = [
+        pokemon.sprites.other["official-artwork"].front_default,
+        pokemon.sprites.back_default
+    ]
+
+    let imageIndex = 0
+
     // set classes
     pokemonCard.setAttribute("class", "card")
     header.setAttribute("class", "card--title")
     image.setAttribute("class", "card--img")
 
+    // set header with name of pokemon
     const name = String(pokemon.name).charAt(0).toUpperCase() + String(pokemon.name).slice(1)
     header.innerText = name
+
     image.setAttribute("width", "256")
-    image.setAttribute("src", pokemon.sprites.other["official-artwork"].front_default)
+    image.setAttribute("src", images[imageIndex])
 
     // add elements to card
     pokemonCard.appendChild(header)
     pokemonCard.appendChild(image)
     pokemonCard.appendChild(createStatList(pokemon.stats))
+
+    // toggle image on click
+    pokemonCard.addEventListener("click", () => {
+        if (imageIndex === 1) {
+            imageIndex = 0
+        } else {
+            imageIndex = 1
+        }
+        image.setAttribute("src", images[imageIndex])
+    })
     
     return pokemonCard
 }
